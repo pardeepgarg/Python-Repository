2803299462e327ae678c70e338aba02ddfc1664c
Pardeep Kumar <pardeepgarg127@gmail.com>
2022-02-16 19:46:00 +0530

    updating new file Breakfast

--- Hello.py
+++ Hello.py
@@ -1,3 +1,9 @@
+from Healthy import Health
+from Breakfast import test
+Health(2,3)
+print('------------------------')
+test()
+print('------------------------')
 print("Hello world!!!")
 print('added git to Visual studio')
 print('added Pardeep branch')
